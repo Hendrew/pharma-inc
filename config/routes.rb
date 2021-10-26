ff171ce8f7f2615cdf87de58ccb01cdc857d0fca
Hendrew <hendrew.developer@gmail.com>
2021-10-26 17:33:56 -0300

    feat: add import route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
+      resources :users, except: :create do
+        get :import, on: :collection
+      end
     end
   end
 end
