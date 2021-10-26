cec062ab65d9da987ed1b6d903666af7c14ae880
Hendrew <hendrew.developer@gmail.com>
2021-10-26 14:34:13 -0300

    feat: add user_locations migration

---
+++ db/migrate/20211026172104_create_user_locations.rb
@@ -0,0 +1,12 @@
+class CreateUserLocations < ActiveRecord::Migration[6.1]
+  def change
+    create_table :user_locations do |t|
+      t.string      :street,    null: false, default: ''
+      t.string      :city,      null: false, default: ''
+      t.string      :postcode,  null: false, default: ''
+      t.references  :user,      null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
