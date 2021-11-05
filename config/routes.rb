3435040bdd4e9707e25a38f97eedf04392286f59
Hendrew <hendrew.developer@gmail.com>
2021-11-04 22:10:04 -0300

    chore: remove import route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, except: :create do
-        get :import, on: :collection
-      end
+      resources :users, except: :create
     end
   end
 end
